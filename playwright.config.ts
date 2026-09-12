a20bcc0bb664fcb1a80b436f5443c6fa7a773a8b
igor <example@gmail.com>
2026-09-12 13:04:50 +0300

    adding another parametr test for toast

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npx serve . -p 5500',
+    url: 'http://localhost:5500',
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from './fixtures';
 
-test('Empty search toast check - all languages (ru, en, la)', async ({ mainPage, page }) => {
+test('Empty search toast check - all languages (ru, en, la)', async ({ mainPage }) => {
   const expectedTextData = [
     { lang: 'ru', text: 'Напиши что-нибудь, грешник!' },
     { lang: 'en', text: 'Write something, sinner!' },
@@ -14,4 +14,18 @@ test('Empty search toast check - all languages (ru, en, la)', async ({ mainPage,
     expect(actualToastText).toContain(text);
     console.log(actualToastText);
   }
+});
+
+[
+  { lang: 'ru', text: 'Напиши что-нибудь, грешник!' },
+  { lang: 'en', text: 'Write something, sinner!' },
+  { lang: 'la', text: 'ALIQUID SCRIBE, PECCATOR!' },
+].forEach(({ lang, text }) => {
+  test(`Empty search toast check with ${lang} language`, async ({ mainPage }) => {
+    await mainPage.header.selectLanguage(lang as 'ru' | 'en' | 'la');
+    await mainPage.searchButton.click();
+    const actualToastText = await mainPage.toast.getToastText();
+    expect(actualToastText).toContain(text);
+    console.log(actualToastText);
+  });
 });
